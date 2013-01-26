2e0bfcc281fe197c4edd8ad07ec52a6b632153b9
Enrique <niofis@gmail.com>
2013-01-25 17:49:15 -0700

    Initial commit.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace DiskCleanUp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstFolders = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDeleteFolder = new System.Windows.Forms.Button();
+            this.btRemoveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder to Scan:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolder.Location = new System.Drawing.Point(12, 25);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(611, 20);
+            this.txtFolder.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(630, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folders created by compressed files:";
+            // 
+            // lstFolders
+            // 
+            this.lstFolders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFolders.FormattingEnabled = true;
+            this.lstFolders.Location = new System.Drawing.Point(12, 106);
+            this.lstFolders.Name = "lstFolders";
+            this.lstFolders.Size = new System.Drawing.Size(570, 134);
+            this.lstFolders.TabIndex = 4;
+            this.lstFolders.SelectedIndexChanged += new System.EventHandler(this.lstFolders_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Possible sources:";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 259);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(570, 121);
+            this.lstFiles.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(504, 55);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(226, 36);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Begin Scan";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnDeleteFolder
+            // 
+            this.btnDeleteFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeleteFolder.Location = new System.Drawing.Point(589, 139);
+            this.btnDeleteFolder.Name = "btnDeleteFolder";
+            this.btnDeleteFolder.Size = new System.Drawing.Size(141, 53);
+            this.btnDeleteFolder.TabIndex = 8;
+            this.btnDeleteFolder.Text = "Remove Folder From Drive";
+            this.btnDeleteFolder.UseVisualStyleBackColor = true;
+            this.btnDeleteFolder.Click += new System.EventHandler(this.btnDeleteFolder_Click);
+            // 
+            // btRemoveFile
+            // 
+            this.btRemoveFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btRemoveFile.Location = new System.Drawing.Point(589, 278);
+            this.btRemoveFile.Name = "btRemoveFile";
+            this.btRemoveFile.Size = new System.Drawing.Size(141, 53);
+            this.btRemoveFile.TabIndex = 9;
+            this.btRemoveFile.Text = "Remove File From Drive";
+            this.btRemoveFile.UseVisualStyleBackColor = true;
+            this.btRemoveFile.Click += new System.EventHandler(this.btRemoveFile_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 394);
+            this.Controls.Add(this.btRemoveFile);
+            this.Controls.Add(this.btnDeleteFolder);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstFolders);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Disk Clean Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstFolders;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnDeleteFolder;
+        private System.Windows.Forms.Button btRemoveFile;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace DiskCleanUp
+{
+    public partial class frmMain : Form
+    {
+        private Regex filename=new Regex("(?<name>[^\\\\/]+?)$",RegexOptions.Multiline | RegexOptions.IgnoreCase);
+        private Regex foldername = new Regex("(?<name>[^\\\\/]+?)[\\\\/]?$", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+
+        private Dictionary<string,List<string>> findings;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbdFolder.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = fbdFolder.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            findings = new Dictionary<string, List<string>>();
+            lstFolders.Items.Clear();
+            lstFiles.Items.Clear();
+            ScanFolder(txtFolder.Text);
+            foreach (string k in findings.Keys)
+            {
+                lstFolders.Items.Add(k);
+            }
+        }
+
+        private void ScanFolder(string directory)
+        {
+            try
+            {
+                List<string> folders = new List<string>();
+                List<string> files = new List<string>();
+                folders.AddRange(System.IO.Directory.GetDirectories(directory));
+                files.AddRange(System.IO.Directory.GetFiles(directory));
+                string f1, f2;
+                Match m1, m2;
+                Regex derived;
+
+                foreach (string folder in folders)
+                {
+                    if (foldername.IsMatch(folder))
+                    {
+                        m1 = foldername.Match(folder);
+                        f1 = m1.Groups["name"].Value;
+                        //derived = new Regex("^"+f1 + "+", RegexOptions.IgnoreCase);
+                        foreach (string file in files)
+                        {
+                            if (filename.IsMatch(file))
+                            {
+                                m2 = filename.Match(file);
+                                f2 = m2.Groups["name"].Value;
+                                if (f2.Length >= f1.Length)
+                                    if (f2.Substring(0, f1.Length).Equals(f1, StringComparison.OrdinalIgnoreCase))
+                                    {
+                                        if (!findings.ContainsKey(folder))
+                                            findings[folder] = new List<string>();
+                                        findings[folder].Add(f2);
+                                    }
+                            }
+                        }
+                    }
+                }
+
+                foreach (string k in findings.Keys)
+                {
+                    folders.Remove(k);
+                }
+
+                foreach (string folder in folders)
+                {
+                    ScanFolder(folder);
+                }
+            }
+            catch { }
+            
+        }
+
+        private void lstFolders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstFiles.Items.Clear();
+            if(lstFolders.SelectedItem!=null)
+            foreach( string file in findings[lstFolders.SelectedItem.ToString()])
+            {
+                lstFiles.Items.Add(file);
+            }
+        }
+
+        private void btnDeleteFolder_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are your sure?","Confirmation",MessageBoxButtons.YesNo,MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                try
+                {
+                    System.IO.Directory.Delete(lstFolders.SelectedItem.ToString(),true);
+                    findings.Remove(lstFolders.SelectedItem.ToString());
+                    lstFolders.Items.Remove(lstFolders.SelectedItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("The folder could not be removed!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void btRemoveFile_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are your sure?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                try
+                {
+                    string file;
+                    file = lstFolders.SelectedItem.ToString() + "/" + lstFiles.SelectedItem.ToString();
+                    System.IO.File.Delete(file);
+                    findings[lstFolders.SelectedItem.ToString()].Remove(lstFiles.SelectedItem.ToString());
+                    lstFiles.Items.Remove(lstFiles.SelectedItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("The file could not be removed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
